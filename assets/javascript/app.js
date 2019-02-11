d7bf47c939d7a2e1ccf9fc2c7b14afff9e9eed12
Moonkitten <lauragreer3@gmail.com>
2019-02-10 22:18:07 -0700

    added stuff

---
+++ assets/javascript/app.js
@@ -0,0 +1,5 @@
+var tFrequency = 15;
+var firstTime = "04:00";
+//convert time back to make sure time is reset to capture actual curent time
+//show difference between current time and arrival time
+//data input recorded to add new trains
