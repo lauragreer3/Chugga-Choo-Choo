36cfcfdd7c6d40bef3baa7563188d63792bfed12
Moonkitten <lauragreer3@gmail.com>
2019-02-11 21:07:52 -0700

    js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,14 @@
+// Initialize Firebase
+var config = {
+apiKey: "AIzaSyBx0wYabvpqSH62XvHfLX-YKAZQgA7dEDQ",
+authDomain: "chugga-choo-choo.firebaseapp.com",
+databaseURL: "https://chugga-choo-choo.firebaseio.com",
+projectId: "chugga-choo-choo",
+storageBucket: "chugga-choo-choo.appspot.com",
+messagingSenderId: "779695021382"
+};
+firebase.initializeApp(config);
+
 var tFrequency = 15;
 var firstTime = "04:00";
 //convert time back to make sure time is reset to capture actual curent time
